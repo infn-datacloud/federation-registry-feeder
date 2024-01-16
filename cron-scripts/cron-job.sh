c7c40af74c7d2f792901ffed2ea818d466265d21
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-01-16 16:35:05 +0100

    Update logging of cron executable

--- cron-scripts/cron-job.sh
+++ cron-scripts/cron-job.sh
@@ -4,4 +4,4 @@ scriptPath=$(dirname "$(readlink -f "$0")")
 . "${scriptPath}/.env.sh"
 
 # the docker-compose variables should be available here
-/usr/local/bin/python /app/src/main.py
+/usr/local/bin/python /app/src/main.py >> $LOG_FILE 2>&1
