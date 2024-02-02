9a48d9b6b489831722b62e7a3b0d8e95fe1031e4
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-02-02 16:47:03 +0100

    Remove cron scripts

--- cron-scripts/cron-job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-scriptPath=$(dirname "$(readlink -f "$0")")
-. "${scriptPath}/.env.sh"
-
-# the docker-compose variables should be available here
-/usr/local/bin/python /app/src/main.py >> $LOG_FILE 2>&1
