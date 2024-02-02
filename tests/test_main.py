d1dc726f3976fc90643fdbbaf2404dc45fae651f
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-02-02 16:35:14 +0100

    Update main tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
+from typing import List
 from unittest.mock import Mock, patch
 
 from app.provider.schemas_extended import ProviderCreateExtended
+from pytest_cases import parametrize_with_cases
 
 from src.config import URLs
 from src.main import main
@@ -8,27 +10,45 @@ from src.models.config import SiteConfig
 from src.models.provider import Kubernetes, Openstack
 
 
+class CaseSiteConfigs:
+    def case_no_config(self) -> List[SiteConfig]:
+        return []
+
+    def case_one_config(
+        self,
+        site_config: SiteConfig,
+        openstack_provider: Openstack,
+        kubernetes_provider: Kubernetes,
+    ) -> List[SiteConfig]:
+        site_config.openstack = [openstack_provider]
+        site_config.kubernetes = [kubernetes_provider]
+        return [site_config]
+
+
 @patch("src.main.update_database")
 @patch("src.main.get_provider")
-@patch("src.utils.load_config")
+@patch("src.main.get_site_configs")
 @patch("src.main.infer_service_endpoints")
+@parametrize_with_cases("site_configs", cases=CaseSiteConfigs)
 def test_main(
     mock_infer_urls: Mock,
     mock_load_conf: Mock,
     mock_get_prov: Mock,
     mock_edit_db: Mock,
     service_endpoints: URLs,
-    site_config: SiteConfig,
-    openstack_provider: Openstack,
-    kubernetes_provider: Kubernetes,
     provider_create: ProviderCreateExtended,
+    site_configs: List[SiteConfig],
 ) -> None:
-    site_config.openstack = [openstack_provider]
-    site_config.kubernetes = [kubernetes_provider]
+    """text the execution of the main function.
 
+    If no exceptions or errors are raised, the main function should be executed till
+    the end.
+    """
     mock_infer_urls.return_value = service_endpoints
-    mock_load_conf.return_value = site_config
+    mock_load_conf.return_value = site_configs
     mock_get_prov.return_value = provider_create
     mock_edit_db.return_value = None
 
     main()
+
+    mock_edit_db.assert_called_once()
