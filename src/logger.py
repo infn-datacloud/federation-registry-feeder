2f8616e93e0262d76c4f192062df24349f731df1
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-07-05 12:46:06 +0000

    Split logs to stderr and stdout

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,34 @@
 import logging
+import sys
+from logging import Formatter, Logger, StreamHandler
 
-logger = logging.getLogger("Federation Registry populator")
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
-logger.addHandler(handler)
+
+class StdoutFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno <= logging.WARNING
+
+
+class StderrFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno >= logging.ERROR
+
+
+def create_logger(name: str, level: str) -> Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    stdout_handler = StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    stdout_handler.addFilter(StdoutFilter())
+    logger.addHandler(stdout_handler)
+
+    stderr_handler = StreamHandler()
+    stderr_handler.setFormatter(formatter)
+    stderr_handler.addFilter(StderrFilter())
+    logger.addHandler(stderr_handler)
+
+    return logger
+
+
+logger = create_logger("Federation Registry populator", level="INFO")
