c1507134cac7c804a5404e376c8994381876735f
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-10-19 11:39:30 +0000

    tests: Update openstack related tests

--- tests/providers/openstack/conftest.py
+++ tests/providers/openstack/conftest.py
@@ -2,9 +2,8 @@ from logging import getLogger
 from unittest.mock import patch
 
 import pytest
-from fed_reg.provider.schemas_extended import IdentityProviderCreateExtended
 
-from src.models.provider import Openstack, Project
+from src.models.provider import Openstack
 from src.providers.openstack import OpenstackData
 from tests.schemas.utils import (
     auth_method_dict,
@@ -15,23 +14,14 @@ from tests.schemas.utils import (
 
 
 @pytest.fixture
-def openstack_item(identity_provider_create: IdentityProviderCreateExtended):
-    project_conf = Project(**project_dict())
+def openstack_item():
     provider_conf = Openstack(
         **openstack_dict(),
         identity_providers=[auth_method_dict()],
-        projects=[project_conf],
+        projects=[project_dict()],
     )
-    region_name = random_lower_string()
     token = random_lower_string()
     logger = getLogger("test")
     with patch("src.providers.openstack.OpenstackData.retrieve_info"):
-        item = OpenstackData(
-            provider_conf=provider_conf,
-            project_conf=project_conf,
-            identity_provider=identity_provider_create,
-            region_name=region_name,
-            token=token,
-            logger=logger,
-        )
+        item = OpenstackData(provider_conf=provider_conf, token=token, logger=logger)
     return item
--- tests/providers/openstack/test_creation.py
+++ tests/providers/openstack/test_creation.py
@@ -1,9 +1,7 @@
 from logging import getLogger
 from unittest.mock import Mock, patch
 
-from fed_reg.provider.schemas_extended import IdentityProviderCreateExtended
-
-from src.models.provider import Openstack, Project
+from src.models.provider import Openstack
 from src.providers.openstack import OpenstackData
 from tests.schemas.utils import (
     auth_method_dict,
@@ -15,33 +13,20 @@ from tests.schemas.utils import (
 
 @patch("src.providers.openstack.OpenstackData.create_connection")
 @patch("src.providers.openstack.OpenstackData.retrieve_info")
-def test_creation(
-    mock_retrieve_info: Mock,
-    mock_create_connection: Mock,
-    identity_provider_create: IdentityProviderCreateExtended,
-) -> None:
-    project_conf = Project(**project_dict())
+def test_creation(mock_retrieve_info: Mock, mock_create_connection: Mock) -> None:
     provider_conf = Openstack(
         **openstack_dict(),
         identity_providers=[auth_method_dict()],
-        projects=[project_conf],
+        projects=[project_dict()],
     )
-    region_name = random_lower_string()
-    logger = getLogger("test")
     token = random_lower_string()
-    item = OpenstackData(
-        provider_conf=provider_conf,
-        project_conf=project_conf,
-        identity_provider=identity_provider_create,
-        region_name=region_name,
-        token=token,
-        logger=logger,
-    )
+    logger = getLogger("test")
+    item = OpenstackData(provider_conf=provider_conf, token=token, logger=logger)
     assert item is not None
     assert item.provider_conf == provider_conf
-    assert item.project_conf == project_conf
-    assert item.identity_provider == identity_provider_create
-    assert item.region_name == region_name
+    assert item.project_conf == provider_conf.projects[0]
+    assert item.auth_method == provider_conf.identity_providers[0]
+    assert item.region_name == provider_conf.regions[0].name
     assert item.logger == logger
     assert item.conn is not None
 
@@ -49,23 +34,18 @@ def test_creation(
     mock_retrieve_info.assert_called_once()
 
 
-def test_connection(identity_provider_create: IdentityProviderCreateExtended) -> None:
+def test_connection() -> None:
     """Connection creation always succeeds, it is its usage that may fail."""
-    project_conf = Project(**project_dict())
     provider_conf = Openstack(
         **openstack_dict(),
         identity_providers=[auth_method_dict()],
-        projects=[project_conf],
+        projects=[project_dict()],
     )
-    region_name = random_lower_string()
-    logger = getLogger("test")
     token = random_lower_string()
+    logger = getLogger("test")
     with patch("src.providers.openstack.OpenstackData.retrieve_info"):
         item = OpenstackData(
             provider_conf=provider_conf,
-            project_conf=project_conf,
-            identity_provider=identity_provider_create,
-            region_name=region_name,
             token=token,
             logger=logger,
         )
@@ -73,11 +53,11 @@ def test_connection(identity_provider_create: IdentityProviderCreateExtended) ->
     assert item.conn.auth.get("auth_url") == provider_conf.auth_url
     assert (
         item.conn.auth.get("identity_provider")
-        == identity_provider_create.relationship.idp_name
+        == provider_conf.identity_providers[0].idp_name
     )
     assert (
-        item.conn.auth.get("protocol") == identity_provider_create.relationship.protocol
+        item.conn.auth.get("protocol") == provider_conf.identity_providers[0].protocol
     )
     assert item.conn.auth.get("access_token") == token
-    assert item.conn.auth.get("project_id") == project_conf.id
-    assert item.conn._compute_region == region_name
+    assert item.conn.auth.get("project_id") == provider_conf.projects[0].id
+    assert item.conn._compute_region == provider_conf.regions[0].name
