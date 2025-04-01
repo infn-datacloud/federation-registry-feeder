b15346a9649bab019e450d9c2a7380f1a7e2230e
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-04-01 15:59:55 +0000

    feat: add new info to the feeder msg sent to kafka

--- src/kafka_conn.py
+++ src/kafka_conn.py
@@ -50,21 +50,25 @@ def send_kafka_messages(
     *, kafka_prod: Producer, connections_data: list[dict[str, Any]]
 ):
     """Organize quotas data and send them to kafka."""
-    msg_version = "1.1.0"
+    msg_version = "1.2.0"
     for data in connections_data:
         provider_conf = data.pop("provider_conf")
         issuer = data.pop("issuer")
         project = data.pop("project")
+        identity_services = data.pop("identity_services")
         message = {
             "msg_version": msg_version,
             "provider_name": provider_conf["name"],
             "provider_type": provider_conf["type"],
+            "identity_endpoint": identity_services[0]["endpoint"],
             "region_name": provider_conf["regions"][0]["name"],
             "overbooking_cpu": provider_conf["regions"][0]["overbooking_cpu"],
             "overbooking_ram": provider_conf["regions"][0]["overbooking_ram"],
             "bandwidth_in": provider_conf["regions"][0]["bandwidth_in"],
             "bandwidth_out": provider_conf["regions"][0]["bandwidth_out"],
             "issuer_endpoint": issuer["endpoint"],
+            "issuer_name": provider_conf["identity_providers"][0]["idp_name"],
+            "issuer_protocol": provider_conf["identity_providers"][0]["protocol"],
             "user_group": issuer["user_groups"][0]["name"],
             "project_id": project["uuid"],
             **data,
