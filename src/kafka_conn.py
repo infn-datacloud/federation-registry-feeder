cc4f0a8c1db472c23a78ec2115d655b56da710f3
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-06-30 12:21:29 +0000

    feat: update kafka msg version to 1.3.0 (add timestamp)

--- src/kafka_conn.py
+++ src/kafka_conn.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from logging import Logger
 from typing import Any
 
@@ -111,24 +112,50 @@ class Producer:
         issuer = data.pop("issuer")
         project = data.pop("project")
         identity_services = data.pop("identity_services")
-        if msg_version == "1.2.0":
-            return {
-                "msg_version": msg_version,
-                "provider_name": provider_conf["name"],
-                "provider_type": provider_conf["type"],
-                "identity_endpoint": identity_services[0]["endpoint"],
-                "region_name": provider_conf["regions"][0]["name"],
-                "overbooking_cpu": provider_conf["regions"][0]["overbooking_cpu"],
-                "overbooking_ram": provider_conf["regions"][0]["overbooking_ram"],
-                "bandwidth_in": provider_conf["regions"][0]["bandwidth_in"],
-                "bandwidth_out": provider_conf["regions"][0]["bandwidth_out"],
-                "issuer_endpoint": issuer["endpoint"],
-                "issuer_name": provider_conf["identity_providers"][0]["idp_name"],
-                "issuer_protocol": provider_conf["identity_providers"][0]["protocol"],
-                "user_group": issuer["user_groups"][0]["name"],
-                "project_id": project["uuid"],
-                **data,
-            }
+        match msg_version:
+            case "1.2.0":
+                return {
+                    "msg_version": msg_version,
+                    "provider_name": provider_conf["name"],
+                    "provider_type": provider_conf["type"],
+                    "identity_endpoint": identity_services[0]["endpoint"],
+                    "region_name": provider_conf["regions"][0]["name"],
+                    "overbooking_cpu": provider_conf["regions"][0]["overbooking_cpu"],
+                    "overbooking_ram": provider_conf["regions"][0]["overbooking_ram"],
+                    "bandwidth_in": provider_conf["regions"][0]["bandwidth_in"],
+                    "bandwidth_out": provider_conf["regions"][0]["bandwidth_out"],
+                    "issuer_endpoint": issuer["endpoint"],
+                    "issuer_name": provider_conf["identity_providers"][0]["idp_name"],
+                    "issuer_protocol": provider_conf["identity_providers"][0][
+                        "protocol"
+                    ],
+                    "user_group": issuer["user_groups"][0]["name"],
+                    "project_id": project["uuid"],
+                    **data,
+                }
+            case "1.3.0":
+                return {
+                    "msg_version": msg_version,
+                    "timestamp": datetime.now(),
+                    "provider_name": provider_conf["name"],
+                    "provider_type": provider_conf["type"],
+                    "identity_endpoint": identity_services[0]["endpoint"],
+                    "region_name": provider_conf["regions"][0]["name"],
+                    "overbooking_cpu": provider_conf["regions"][0]["overbooking_cpu"],
+                    "overbooking_ram": provider_conf["regions"][0]["overbooking_ram"],
+                    "bandwidth_in": provider_conf["regions"][0]["bandwidth_in"],
+                    "bandwidth_out": provider_conf["regions"][0]["bandwidth_out"],
+                    "issuer_endpoint": issuer["endpoint"],
+                    "issuer_name": provider_conf["identity_providers"][0]["idp_name"],
+                    "issuer_protocol": provider_conf["identity_providers"][0][
+                        "protocol"
+                    ],
+                    "user_group": issuer["user_groups"][0]["name"],
+                    "project_id": project["uuid"],
+                    **data,
+                }
+            case _:
+                raise ValueError("Invalid message version: %s", msg_version)
 
 
 def send_to_kafka(
