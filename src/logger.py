a447b4e211bcd4abf26a66ce8554469402330b9a
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2026-06-30 08:15:06 +0200

    Add thread-id to logging

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import threading
 from logging import Formatter, Logger, StreamHandler
 
 
@@ -11,6 +12,12 @@ class StdoutFilter(logging.Filter):
 class StderrFilter(logging.Filter):
     def filter(self, record):
         return record.levelno >= logging.ERROR
+
+
+def thread_id_filter(record):
+    """Inject thread_id to log records"""
+    record.thread_id = threading.get_native_id()
+    return record
 
 
 def create_logger(name: str, level: str | int | None = None) -> Logger:
@@ -26,15 +33,19 @@ def create_logger(name: str, level: str | int | None = None) -> Logger:
         error_msg = None
     except ValueError:
         error_msg = f"Invalid log level: {level}"
-    formatter = Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    formatter = Formatter(
+        "%(asctime)s - %(levelname)s - %(thread_id)d - %(name)s - %(message)s"
+    )
 
     stdout_handler = StreamHandler(sys.stdout)
     stdout_handler.setFormatter(formatter)
+    stdout_handler.addFilter(thread_id_filter)
     stdout_handler.addFilter(StdoutFilter())
     logger.addHandler(stdout_handler)
 
     stderr_handler = StreamHandler()
     stderr_handler.setFormatter(formatter)
+    stdout_handler.addFilter(thread_id_filter)
     stderr_handler.addFilter(StderrFilter())
     logger.addHandler(stderr_handler)
 
